b717ac270329a37487d840f54cb9bf545aa565bf
Amanda Quirk <acquirk@ucsc.edu>
2018-07-25 15:40:55 -0700

    converted CO velocity from LSR to heliocentric

--- matching_CO.py
+++ matching_CO.py
@@ -52,7 +52,7 @@ CO_eta=m31_coord(RA_good, Dec_good, x=False)
 
 #importing the star data
 #xi (kpc), eta (kpc), average v(km/s), v err,var, n, HI main, HI close <-- header of data file to be read in 
-RG_xi, RG_eta=np.loadtxt('/Users/amandaquirk/Documents/AsymmetricDrift/Data/RG_smoothed_chemin.txt', usecols=(0,1,), unpack=True)
+MS_xi, MS_eta=np.loadtxt('/Users/amandaquirk/Documents/AsymmetricDrift/Data/MS_smoothed_chemin.txt', usecols=(0,1,), unpack=True)
 
 #line to see where CO data ends
 def y(x):
@@ -61,7 +61,7 @@ def y(x):
 x = np.linspace(0,13, 10)
 
 plt.scatter(CO_xi, CO_eta, c=CO_v_good)
-im = plt.scatter(RG_xi, RG_eta, c= 'b', alpha=0.4)
+im = plt.scatter(MS_xi, MS_eta, c= 'b', alpha=0.4)
 plt.plot(x, y(x))
 plt.gca().invert_xaxis()
 plt.xlabel('xi (kpc)')
@@ -70,32 +70,49 @@ plt.show()
 plt.close()
 
 #truncating the star data to just inside the CO field
-CO_field = RG_eta < 16.8 - 0.8 * RG_xi
-RG_xi = RG_xi[CO_field]
-RG_eta = RG_eta[CO_field]
+CO_field = MS_eta < 16.8 - 0.8 * MS_xi
+MS_xi = MS_xi[CO_field]
+MS_eta = MS_eta[CO_field]
 
 #turning xi and eta into a coordinate pair
 def coords(xi, eta):
 	return SkyCoord(xi/13.67, eta/13.67, unit=(u.deg, u.deg)) #dividing by 13.67 to convert to degrees
 
 CO_coords=coords(CO_xi, CO_eta)
-RG_coords=coords(RG_xi, RG_eta)
+MS_coords=coords(MS_xi, MS_eta)
 
 #function finds the closest CO
 def find_closest(star, data):
 	idx,d2d,d3d=star.match_to_catalog_sky(data)
 	return idx
 
-indices = find_closest(RG_coords, CO_coords)
-RG_CO_v = []
-for i in indices:
-	RG_CO_v.append(CO_v_good[i])
+indices = find_closest(MS_coords, CO_coords)
 
-plt.scatter(RG_xi, RG_eta, c=RG_CO_v)
+#write oMSinial coordinates of CO
+def CO_coords_deg(RA, Dec, x=True):
+	c = SkyCoord(RA, Dec, unit=(u.deg, u.deg))
+	if x==True:
+		return c.ra.deg 
+	if x==False:
+		return c.dec.deg
+
+CO_RA = CO_coords_deg(RA_good, Dec_good, x=True) 
+CO_Dec = CO_coords_deg(RA_good, Dec_good, x=False)
+
+
+MS_CO_v = []
+MS_RA = []
+MS_Dec = []
+for i in indices:
+	MS_CO_v.append(CO_v_good[i])
+	MS_RA.append(CO_RA[i])
+	MS_Dec.append(CO_Dec[i])
+
+plt.scatter(MS_RA, MS_Dec, c=MS_CO_v)
 plt.gca().invert_xaxis()
 plt.xlabel('xi (kpc)')
 plt.ylabel('eta (kpc)')
 plt.colorbar()
 plt.show()
 
-np.savetxt('/Users/amandaquirk/Desktop/RG_CO.txt', np.c_[RG_xi, RG_eta, RG_CO_v], fmt='%1.16f', delimiter=' ', header='xi (kpc), eta (kpc), CO v')
+np.savetxt('/Users/amandaquirk/Desktop/MS_CO.txt', np.c_[MS_xi, MS_eta, MS_CO_v, MS_RA, MS_Dec], fmt='%1.16f', delimiter=' ', header='xi (kpc), eta (kpc), CO v, RA (deg), Dec (deg)')
